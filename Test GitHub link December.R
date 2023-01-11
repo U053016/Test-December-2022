a6c39c9dfb5200ea88430bc126fd3e6c58004a13
U053016 <blaise.wassmer@ucb.com>
2023-01-11 14:30:57 +0100

    test

--- Test GitHub link December.R
+++ Test GitHub link December.R
@@ -1,2 +1,10 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
+Myfunction <- function() {
+  x <- rnorm(100)
+  mean(x)
+}
+Myfunction2 <- function() {
+  x <- rnorm(100)
+  mean(x)
+}
